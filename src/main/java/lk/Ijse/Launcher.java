e31eff5b5907ed881cb9b24cd6eb21ec000bb752
nimsimethusala <nimsimethusala42@gmail.com>
2024-04-18 23:30:37 +0530

    commit

---
+++ src/main/java/lk/Ijse/Launcher.java
@@ -0,0 +1,22 @@
+package lk.Ijse;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        AnchorPane root = FXMLLoader.load(getClass().getResource("/view/loginForm.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
+}
