93cb4eaa4a6f6a9b0b134286c69ce6691f465d3c
nimsimethusala <nimsimethusala42@gmail.com>
2024-04-20 02:04:22 +0530

    dashboard

--- src/main/java/lk/Ijse/controller/dashboardFormController.java
+++ src/main/java/lk/Ijse/controller/dashboardFormController.java
@@ -2,11 +2,17 @@ package lk.Ijse.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
 
 public class dashboardFormController {
 
@@ -20,7 +26,7 @@ public class dashboardFormController {
     private Label lblJobCount;
 
     @FXML
-    private AnchorPane root;
+    private AnchorPane Root;
 
     @FXML
     private TableColumn<?, ?> tblCustomerName;
@@ -39,37 +45,93 @@ public class dashboardFormController {
 
     @FXML
     void btnCustomerOnAction(ActionEvent event) {
+        try {
+            AnchorPane root = FXMLLoader.load(getClass().getResource("/view/customerForm.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) dashboardRoot.getScene().getWindow();
+            stage.setScene(scene);
 
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     void btnDefectOnAction(ActionEvent event) {
+        try {
+            AnchorPane root = FXMLLoader.load(getClass().getResource("/view/defectForm.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) dashboardRoot.getScene().getWindow();
+            stage.setScene(scene);
 
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     void btnEmployeeOnAction(ActionEvent event) {
+        try {
+            AnchorPane root = FXMLLoader.load(getClass().getResource("/view/employeeForm.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) dashboardRoot.getScene().getWindow();
+            stage.setScene(scene);
 
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     void btnItemOnAction(ActionEvent event) {
+        try {
+            AnchorPane root = FXMLLoader.load(getClass().getResource("/view/itemForm.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) dashboardRoot.getScene().getWindow();
+            stage.setScene(scene);
 
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     void btnNewJobOnAction(ActionEvent event) {
+        try {
+            AnchorPane root = FXMLLoader.load(getClass().getResource("/view/jobForm.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) dashboardRoot.getScene().getWindow();
+            stage.setScene(scene);
 
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     void btnSparesOnAction(ActionEvent event) {
+        try {
+            AnchorPane root = FXMLLoader.load(getClass().getResource("/view/spareForm.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) dashboardRoot.getScene().getWindow();
+            stage.setScene(scene);
 
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     void btnSupplierOnAction(ActionEvent event) {
+        try {
+            AnchorPane root = FXMLLoader.load(getClass().getResource("/view/supplierForm.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) dashboardRoot.getScene().getWindow();
+            stage.setScene(scene);
 
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
@@ -77,4 +139,16 @@ public class dashboardFormController {
 
     }
 
+    @FXML
+    void btnBackOnAction(ActionEvent actionEvent) {
+        try {
+            AnchorPane root = FXMLLoader.load(getClass().getResource("/view/loginForm.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) Root.getScene().getWindow();
+            stage.setScene(scene);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
