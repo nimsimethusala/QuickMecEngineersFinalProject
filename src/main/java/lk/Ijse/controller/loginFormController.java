ab6349c748e5748ae7cd2a67c1fc3fc2af82210e
nimsimethusala <nimsimethusala42@gmail.com>
2024-04-19 22:23:14 +0530

    login form and password form

--- src/main/java/lk/Ijse/controller/loginFormController.java
+++ src/main/java/lk/Ijse/controller/loginFormController.java
@@ -4,16 +4,21 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import lk.Ijse.db.DB;
+import lk.Ijse.db.DbConnection;
 
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class loginFormController extends DB {
+public class loginFormController {
 
     @FXML
     private AnchorPane rootLogin;
@@ -26,19 +31,45 @@ public class loginFormController extends DB {
 
     @FXML
     void btnLoginOnAction(ActionEvent event) {
-        try {
-            String password = txtPassword.getText();
-            String username = txtUserId.getText();
+        String password = txtPassword.getText();
+        String username = txtUserId.getText();
 
-            DB db = new DB();
-            boolean correct = db.verifyCredentials(password, username);
-            if (correct){
-                AnchorPane root = FXMLLoader.load(getClass().getResource("/view/dashboardForm.fxml"));
-                Scene scene = new Scene(root);
-                Stage stage = new Stage();
-                stage.setScene(scene);
-                stage.show();
+        checkCredential(password, username);
+    }
+
+    private void checkCredential(String password, String username) {
+        String sql = "SELECT username, password from credential where username = ?";
+
+        try {
+            Connection connection = DbConnection.getInstance().getConnection();
+            PreparedStatement pstm = connection.prepareStatement(sql);
+            pstm.setObject(1, username);
+            ResultSet resultSet = pstm.executeQuery();
+
+            if (resultSet.next()){
+                String pw = resultSet.getString("password");
+
+                if (password.equals(pw)){
+                    loginDashboard();
+                }else {
+                    new Alert(Alert.AlertType.ERROR,"Sorry! Your Password is incorrect").show();
+                }
+            }else {
+                new Alert(Alert.AlertType.ERROR,"Sorry! Your Username is incorrect").show();
             }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void loginDashboard() {
+        try {
+            AnchorPane root = FXMLLoader.load(getClass().getResource("/view/dashboardForm.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) rootLogin.getScene().getWindow();
+            stage.setScene(scene);
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -49,9 +80,8 @@ public class loginFormController extends DB {
         try {
             AnchorPane root = FXMLLoader.load(getClass().getResource("/view/passwordForm.fxml"));
             Scene scene = new Scene(root);
-            Stage stage = new Stage();
+            Stage stage = (Stage) rootLogin.getScene().getWindow();
             stage.setScene(scene);
-            stage.show();
 
         } catch (IOException e) {
             throw new RuntimeException(e);
--- src/main/java/lk/Ijse/db/DbConnection.java
+++ src/main/java/lk/Ijse/db/DbConnection.java
@@ -13,7 +13,7 @@ public class DbConnection {
         connection = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/QuickMecEngineers",
                 "root",
-                "Ijse123"
+                "Ijse@123"
         );
     }
 
