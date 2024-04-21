0f46ca0950a5f234ab0522412be36838f065b439
nimsimethusala <nimsimethusala42@gmail.com>
2024-04-22 00:26:39 +0530

    customer form complete

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -9,10 +9,6 @@ create table customer(
                          contact varchar(10)
 );
 
-insert into customer values('C001','Kamal','Panadura',0775648952);
-insert into customer values('C002','Namal','Galle',0775679952);
-insert into customer values('C003','Amal','Piiliyandala',0772456871);
-
 create table job(
                     job_No varchar(6)primary key,
                     Model varchar(25),
@@ -20,10 +16,6 @@ create table job(
                     customer_id varchar(6),
                     foreign key(customer_id) references customer(customer_id) on update cascade on delete cascade
 );
-
-insert into job values('J001','Lorry','2022/10/25','C001');
-insert into job values('J002','Bus','2022/10/28','C002');
-insert into job values('J003','Car','2022/11/05','C003');
 
 create table item(
                      item_No varchar(6)primary key,
@@ -33,17 +25,10 @@ create table item(
                      foreign key(job_No)references job(job_No) on update cascade on delete cascade
 );
 
-insert into item values('I001','Piston',2,'J001');
-insert into item values('I002','Piston',2,'J002');
-insert into item values('I003','Piston',2,'J003');
-
 create table defect(
                        defect_id varchar(6)primary key,
                        description varchar(25)
 );
-
-insert into defect values('D001','reduce piston from top');
-insert into defect values('D002','removing and refitting cylinder liner');
 
 create table item_defect_details(
                                     item_No varchar(6),
@@ -51,9 +36,6 @@ create table item_defect_details(
                                     defect_id varchar(6),
                                     foreign key(defect_id)references defect(defect_id) on update cascade on delete cascade
 );
-
-insert into item_defect_details values('I001','D001');
-insert into item_defect_details values('I002','D002');
 
 create table employee(
                          Emp_id varchar(6)primary key,
